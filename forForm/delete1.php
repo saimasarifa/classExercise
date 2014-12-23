fcfd2a004810b1f73c94513bf886525f6745bc64
saimasarifa <saimasarifa@users.noreply.github.com>
2014-12-23 13:39:27 +0600

    Update delete1.php

--- forForm/delete1.php
+++ forForm/delete1.php
@@ -1,14 +1,5 @@
 <?php
-<<<<<<< HEAD
-/**
- * Created by PhpStorm.
- * User: lict
- * Date: 12/22/14
- * Time: 4:33 PM
- */
-=======
 
->>>>>>> 3d79987354c924685bf422f6e60814a380760b85
 $id = $_GET['ID'];
 
 $link = mysqli_connect("localhost",
@@ -20,9 +11,6 @@ $query = "DELETE FROM `students`.`users` WHERE `users`.`ID` = $id";
 
 mysqli_query($link, $query);
 
-<<<<<<< HEAD
 header('location:list1.php');
-=======
-header('location:list1.php');
+
 ?>
->>>>>>> 3d79987354c924685bf422f6e60814a380760b85
